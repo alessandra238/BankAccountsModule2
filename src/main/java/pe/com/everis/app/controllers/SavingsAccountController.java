8838e93571f05edfc2df3b0204c974e43658ed52
alessandra238 <arosadov@everis.com>
2020-07-23 22:35:36 -0500

    Actualizacion de Controllers Y services

--- src/main/java/pe/com/everis/app/controllers/SavingsAccountController.java
+++ src/main/java/pe/com/everis/app/controllers/SavingsAccountController.java
@@ -1,24 +1,44 @@
 package pe.com.everis.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import pe.com.everis.app.models.dao.SavingsAccountDao;
 import pe.com.everis.app.models.documents.SavingsAccount;
+import pe.com.everis.app.services.SavingsAccountService;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-@Controller
+@RestController
+@RequestMapping("/api/SavingsAccount")
 public class SavingsAccountController {
 
-	//Inyectando el DAO
 	@Autowired
-	private SavingsAccountDao savingsAccountDao;
+	private SavingsAccountService savingsAccountService;
 	
-	@GetMapping("/toList")
-	public String toList(Model model) {
-		Flux<SavingsAccount> savingsAccount = savingsAccountDao.findAll();
-		return "toList";
+	@GetMapping
+	public Mono<ResponseEntity<Flux<SavingsAccount>>> findAll(){
+		return Mono.just(
+				ResponseEntity
+				.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(savingsAccountService.findAllSavingsAccount())
+				);
 	}
+	
+	@GetMapping("/{id}")
+	public Mono<ResponseEntity<SavingsAccount>> findById(@PathVariable String id){
+		return savingsAccountService.findByIdSavingsAccount(id).map( t->
+				ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(t)
+				).defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+	
+	
 }
--- src/main/java/pe/com/everis/app/services/SavingsAccountService.java
+++ src/main/java/pe/com/everis/app/services/SavingsAccountService.java
@@ -2,9 +2,13 @@ package pe.com.everis.app.services;
 
 import pe.com.everis.app.models.documents.SavingsAccount;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface SavingsAccountService {
 
 	Flux<SavingsAccount> findAllSavingsAccount();
 	
+	Mono<SavingsAccount> saveSavingsAccount(SavingsAccount savingsAccount);
+	
+	Mono<SavingsAccount> findByIdSavingsAccount(String id);
 }
